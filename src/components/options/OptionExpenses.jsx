5578655c808e9345d5bcaedd35d7a39caa03e6a1
CIGR2021 <appencigr@gmail.com>
2022-03-20 14:06:28 -0300

    implementação dos options

---
+++ src/components/options/OptionExpenses.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function OptionExpenses() {
+  return (
+    <>
+      <option selected>-</option>
+      <option>Alimentação</option>
+      <option>Lazer</option>
+      <option>Trabalho</option>
+      <option>Transporte</option>
+      <option>Saúde</option>
+    </>
+  )
+}
+
+export default OptionExpenses;
