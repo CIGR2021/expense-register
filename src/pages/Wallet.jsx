af7e9b7ef2857729457a35dc86dec91672501096
CIGR2021 <appencigr@gmail.com>
2022-03-20 16:26:53 -0300

    implementação da página Wallet

---
+++ src/pages/Wallet.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Form from '../components/Form.jsx';
+import Header from '../components/Header.jsx';
+import PaymentList from '../components/PaymentList.jsx';
+
+function Wallet() {
+  return (
+    <>
+      <Header />
+      <Form />
+      <PaymentList />
+    </>
+  );
+}
+
+export default Wallet;
